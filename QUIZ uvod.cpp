ce029f9d8711c32ec4c126c95552cb74590dd455
KristianMazur <75983320+KristianMazur@users.noreply.github.com>
2021-01-10 15:39:31 +0100

    Add files via upload

---
+++ QUIZ uvod.cpp
@@ -0,0 +1,53 @@
+//Kristián Mazúr 2.AI
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "                                                                    " << endl;
+	cout << "     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << endl;
+	cout << "     *                                                             *" << endl;
+	cout << "     *                 * WELCOME TO THE QUIZ *                     *" << endl;
+	cout << "     *                                                             *" << endl;
+	cout << "     *                                                             *" << endl;
+	cout << "     *            ====================================             *" << endl;
+	cout << "     *                Developed By Kristian Mazur                  *" << endl;
+	cout << "     *            ====================================             *" << endl;
+	cout << "     * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *" << endl;
+	cout << "                                                                    " << endl;
+	
+
+	cout << "Zmackni Enter pre start .. " << endl;
+	cin.get();
+
+	string meno;
+	int vek;
+
+	cout << "Ake je vase meno?" << endl;
+	cin >> meno;
+	cout << endl;
+	cout << "Kolko mate rokov?" << endl;
+	cin >> vek;
+	cout << endl;
+	string odpoved;
+	cout << "Ste pripraveny na Quiz " << meno << "? ano/nie" << endl;
+	cin >> odpoved;
+	if (odpoved == "ano")
+	{
+		cout << endl;
+		cout << "Prajem vam vela stastia" << endl;
+	}
+	else {
+		cout << "Dobre dovidenia" << endl;
+		return 0;
+	}
+}
+
+
+
+
+	
